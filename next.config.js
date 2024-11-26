2db4c65099b49332a746e1640ecb280c13b701e8
johnnymcoy <cbuccioljr@gmail.com>
2024-11-26 11:33:01 +1100

    Added in iframeCompatability

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ const ContentSecurityPolicy = `
   media-src 'self';
   connect-src *;
   font-src 'self';
-  frame-src giscus.app
+  frame-src giscus.app https://curtisbucciol.com https://buccigames.com;
 `
 
 const securityHeaders = [
@@ -28,7 +28,7 @@ const securityHeaders = [
   // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
   {
     key: 'X-Frame-Options',
-    value: 'DENY',
+    value: 'SAMEORIGIN',
   },
   // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
   {
